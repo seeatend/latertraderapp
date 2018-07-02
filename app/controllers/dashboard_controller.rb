92d6a467af9e3b350d81cf89de0ef42c0f2697bb
Intrix <tv@intrixtech.com>
2018-07-02 13:24:26 +0530

    b2b order

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,12 @@ class DashboardController < ApplicationController
   protect_from_forgery prepend: true, with: :exception
 	before_action :authenticate_user!
 
+
+	def index
+		 @seller = Seller.where(:seller_email => current_user.email).first
+		@stocks = Stock.where(:seller_id => @seller.id)
+		@offers = Offer.where(:seller_id => @seller.id)
+		@credits = Credit.all
+	end
+
 end
